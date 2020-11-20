154f513b73c56312e1db9f3f52845ec2a2b70047
JADLI AISSAM <hesokaslave@gmail.com>
2020-11-20 16:21:53 +0100

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -35,7 +35,7 @@ function App() {
   const socket = useRef();
 
   useEffect(() => {
-    socket.current =  io.connect('/');
+    socket.current =  io.connect('https://aqueous-garden-74022.herokuapp.com/');
     navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
       setStream(stream);
       if (userVideo.current) {
