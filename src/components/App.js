977f0a0b6caa26c4cea730202040b9569d263476
JADLI AISSAM <hesokaslave@gmail.com>
2020-11-20 16:34:44 +0100

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -66,13 +66,13 @@ function App() {
         iceServers: [
             {
                 urls: "stun:numb.viagenie.ca",
-                username: "sultan1640@gmail.com",
-                credential: "98376683"
+                username: "hesokaslave@gmail.com",
+                credential: "022370133"
             },
             {
                 urls: "turn:numb.viagenie.ca",
-                username: "sultan1640@gmail.com",
-                credential: "98376683"
+                username: "hesokaslave@gmail.com",
+                credential: "022370133"
             }
         ]
     },
